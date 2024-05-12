90af6740c862be729b6d476d4dd20b0dc213c924
iatovar90 <60189953+iatovar90@users.noreply.github.com>
2024-05-12 03:56:29 +0000

    homework_1

--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -5,7 +5,7 @@ The file at /src/homework/b_in_proc_out/output has
 the function get_number.
 '''
 from src.homework.b_in_proc_out.output import get_number, multiply_numbers
-
+from src.homework.b_in_proc_out.output import multiply_numbers
 class Test_Config(unittest.TestCase):
 
     def test_get_number_1(self):
